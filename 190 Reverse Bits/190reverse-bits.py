a85e4d99cf30ff4086e5e2f260e20f04fdfa72b3
Chettiar Karthik Ramachandran <karthikrchettiar2005@gmail.com>
2025-03-11 13:25:10 +0000

    Code challenge submission for question: 190 Reverse Bits

---
+++ 190 Reverse Bits/190reverse-bits.py
@@ -0,0 +1,17 @@
+import math
+class Solution:
+    # @param n, an integer
+    # @return an integer
+    def reverseBits(self, n):
+        
+        
+        res=0
+        for i in range(32):
+            bit=n&1
+            res=(res<<1)|bit
+            n=n>>1
+
+        return res
+
+
+        

281f880d8bc93416ac92785361ea8d5aed8b105f
Chettiar Karthik Ramachandran <karthikrchettiar2005@gmail.com>
2025-08-22 13:26:41 +0000

    Code challenge submission for question: 49 Group Anagrams

---
+++ 49 Group Anagrams/49group-anagrams.py
@@ -0,0 +1,33 @@
+class Solution(object):
+    def groupAnagrams(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: List[List[str]]
+        """
+        strs2=[]
+        done=[0]*len(strs)
+
+        ret=[]
+        
+        for i in range(len(strs)):
+            strs2.append(''.join(sorted(strs[i])))
+        
+        for i in range(len(strs)):
+            listtemp=[]
+            for j in range(i+1,len(strs)):
+                if not done[i] and (strs2[i]==strs2[j]):
+                    done[j]=1
+                    listtemp.append(strs[j])
+            if(not done[i]):
+                listtemp.append(strs[i])
+                done[i]=1
+            if len(listtemp)!=0:
+                ret.append(listtemp)
+
+
+        return ret
+    
+        
+        
+  
+       

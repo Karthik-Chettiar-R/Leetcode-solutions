54128416b6d3e401e985f942f8da5faa312d7826
Chettiar Karthik Ramachandran <karthikrchettiar2005@gmail.com>
2025-12-02 17:25:28 +0000

    Code challenge submission for question: 70 Climbing Stairs

---
+++ 70 Climbing Stairs/70climbing-stairs.py
@@ -0,0 +1,20 @@
+
+
+class Solution(object):
+    def climbStairs(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        if n==1:
+            return 1
+        if n==2:
+            return 2
+        dp=[]
+        dp.append(1)
+        dp.append(2)
+        for i in range(2,n):
+            dp.append(dp[i-1]+dp[i-2])
+        return dp[n-1]
+        
+        

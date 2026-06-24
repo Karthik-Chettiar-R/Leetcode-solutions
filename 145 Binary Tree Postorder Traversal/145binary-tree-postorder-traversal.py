e79aeb79aa4bc031a7f4a55b06ba365bf120e249
Chettiar Karthik Ramachandran <karthikrchettiar2005@gmail.com>
2026-06-24 05:35:53 +0000

    Code challenge submission for question: 145 Binary Tree Postorder Traversal

---
+++ 145 Binary Tree Postorder Traversal/145binary-tree-postorder-traversal.py
@@ -0,0 +1,22 @@
+def postorder(root,ret):
+    if root:
+        postorder(root.left,ret)
+        postorder(root.right,ret)
+        ret.append(root.val)
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def postorderTraversal(self, root):
+        """
+        :type root: Optional[TreeNode]
+        :rtype: List[int]
+        """
+        ret=[]
+        postorder(root,ret)
+        return ret
+        

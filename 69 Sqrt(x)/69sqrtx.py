2c84f83c25e6b806be785d1baa8edd376cf58b42
Chettiar Karthik Ramachandran <karthikrchettiar2005@gmail.com>
2026-06-25 09:36:15 +0000

    Code challenge submission for question: 69 Sqrt(x)

---
+++ 69 Sqrt(x)/69sqrtx.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def mySqrt(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+
+        if x==0:
+            return 0
+        if x==1:
+            return 1
+        left=0
+        right=x
+        while(left<=right):
+            mid=(left+right)/2
+            if mid*mid==x:
+                return mid
+            elif mid*mid<x:
+                left=mid+1
+            else :
+                right=mid-1
+        return right

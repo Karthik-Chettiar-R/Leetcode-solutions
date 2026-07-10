1318be50977de3110d39336ccba7072f0836ce3c
Chettiar Karthik Ramachandran <155309018+Karthik-Chettiar-R@users.noreply.github.com>
2026-07-10 19:02:51 +0530

    Time: 723 ms (27.27%) | Memory: 45.3 MB (45.45%) - LeetSync

--- 3838-path-existence-queries-in-a-graph-i/path-existence-queries-in-a-graph-i.py
+++ 3838-path-existence-queries-in-a-graph-i/path-existence-queries-in-a-graph-i.py
@@ -1,3 +1,23 @@
+class DSU:
+    def __init__(self,nums):
+        self.parent=[i for i in range(len(nums))]
+
+    def find(self,x):
+        if self.parent[x]!=x:
+            self.parent[x]=self.find(self.parent[x])
+
+        return self.parent[x]
+
+    def union(self,x,y):
+        rootX=self.find(x)
+        rootY=self.find(y)
+
+        if rootX==rootY:
+            return False
+        else:
+            self.parent[rootY]=rootX
+            return True
+
 class Solution(object):
     def pathExistenceQueries(self, n, nums, maxDiff, queries):
         """
@@ -8,41 +28,28 @@ class Solution(object):
         :rtype: List[bool]
         """
 
-        ret=[False for i in range(len(queries))]
-        i=0
-        groups=[]
-        curr=set()
-        while(i<len(nums)-1):
-            
-            if abs(nums[i]-nums[i+1])<=maxDiff:
-                curr.add(i)
-                curr.add(i+1)
+        dsu=DSU(nums)
+
+        i=1
+
+        while(i<len(nums)):
+            if abs(nums[i-1]-nums[i])<=maxDiff:
+                dsu.union(i-1,i)
                 i+=1
             else:
-                if curr:
-
-                    groups.append(curr)
-                curr=set()
                 i+=1
-        if curr:
-            groups.append(curr)
 
-        
+        output=[False for i in range(len(queries))]
 
-        for j in range(len(queries)):
-            if queries[j][0]==queries[j][1]:
-                ret[j]=True
+        for i in range(len(queries)):
+            if dsu.find(queries[i][0])==dsu.find(queries[i][1]):
+                output[i]=True
+            else:
                 continue
-            for s in groups:
-                if queries[j][0] in s and queries[j][1] in s:
-                    ret[j]=True
-                    break
-            
         
-        return ret
-            
-                
+        return output
 
-            
+
+
 
         
